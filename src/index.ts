936d8f10136b4069fb997054bc6ad3187aad62d7
kianenigma <kian@parity.io>
2021-12-09 10:42:48 +0330

    final touches

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import * as currencyFormatter from "currency-formatter";
 import { readFileSync } from "fs";
 
 interface PerAccount {
-	account: string,
-	token: string,
+	account_name: string,
+	token_name: string,
 	price: string,
 	in_account: string,
 	in_account_eur: string,
@@ -18,7 +18,7 @@ interface PerAccount {
 	crowdloan_amount_eur: string,
 	total_amount: string,
 	total_amount_eur: string,
-	assets: [string, string, string][]
+	assets: string[]
 }
 
 const SUBSTRATE_BASED_CHAINS = JSON.parse(readFileSync('accounts.json').toString())
@@ -57,10 +57,11 @@ async function main() {
 		const { chain, stashes, overwrite_currency_name }: { chain: string, overwrite_currency_name?: string, stashes: [string, string] } = SUBSTRATE_BASED_CHAINS[uri];
 		const provider = new WsProvider(uri);
 		const api = await ApiPromise.create({ provider });
-		const priceCent = await priceOf(overwrite_currency_name ? overwrite_currency_name : chain.toLowerCase());
-		console.log(`✅ Connected to node: ${uri}  ${chain} [ss58: ${api.registry.chainSS58}][current price :${priceCent} cents]`);
-
+		const token_name = overwrite_currency_name ? overwrite_currency_name : chain.toLowerCase();
+		const priceCent = await priceOf(token_name);
 		const number = (await api.rpc.chain.getBlock()).block.header.number.toBn();
+		console.log(`✅ Connected to node: ${uri} / chain ${chain} / tokens ${api.registry.chainTokens} / at #${number}[ss58: ${api.registry.chainSS58}][current price :${priceCent} cents]`);
+
 
 		const perAccounts = [];
 		for (const [account, name] of stashes) {
@@ -106,7 +107,7 @@ async function main() {
 			}
 
 			// assets
-			const assets: [string, string, string][] = [];
+			const assets: string[] = [];
 			if (api.query.assets) {
 				const allAssetIds = (await api.query.assets.asset.entries()).map((a) => a[0].args[0]);
 				for (const assetId of allAssetIds) {
@@ -117,14 +118,14 @@ async function main() {
 						const assetName = (meta.symbol.toHuman() || "").toString().toLowerCase() ;
 						const assetPriceCent = await priceOf(assetName);
 						const value = valueOfWithDecimals(api, assetAccount.balance, assetPriceCent, decimals);
-						assets.push([assetName, assetAccount.balance.toHuman(), formatEUR(value)])
+						assets.push(`${assetName}, ${assetAccount.balance.toHuman()}, ${formatEUR(value)}`)
 					}
 				}
 			}
 
 			const perAccount: PerAccount = {
-				account: name,
-				token: name,
+				account_name: name,
+				token_name,
 				price: (priceCent.toNumber() / 100).toFixed(2),
 				in_account: balance(api, amount),
 				in_account_eur: formatEUR(valueOf(api, amount, priceCent)),
@@ -134,10 +135,11 @@ async function main() {
 				vested_amount_eur: formatEUR(valueOf(api, vested, priceCent)),
 				total_amount: balance(api, amount.add(crowdloan)),
 				total_amount_eur: formatEUR(valueOf(api, amount.add(crowdloan), priceCent)),
-				assets: assets
+				assets,
 			}
 			perAccounts.push(perAccount);
 		}
+
 		console.table(perAccounts)
 		const assetData = perAccounts.map((x) => x.assets).filter((x) => x.length);
 		if (assetData.length) {

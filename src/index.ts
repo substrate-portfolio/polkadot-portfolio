b03c1ec2e679470e622b2a792925730411e539fa
kianenigma <kian@parity.io>
2022-01-15 17:07:19 +0000

    major revamp

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,50 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
+import '@polkadot/api-augment';
 import { ParaId } from "@polkadot/types/interfaces"
+import { PalletAssetsAssetAccount } from "@polkadot/types/lookup"
 import { BN } from "@polkadot/util";
 import { strict as assert } from "assert/strict";
 import axios from "axios";
 import * as currencyFormatter from "currency-formatter";
 import { readFileSync } from "fs";
 
+import { typesBundlePre900 } from "moonbeam-types-bundle"
+
+
+interface Chain {
+	chain: string,
+	overwrite_currency_name?: string,
+	stashes: [string, string],
+	pallets: string[]
+}
+
+interface Asset {
+	amount: BN,
+	transferrable: boolean,
+	name: string,
+	price_cents: BN,
+	decimals: BN,
+}
+
+interface PerPallet {
+	name: string,
+	assets: Asset[]
+}
+
 interface PerAccount {
-	account_name: string,
-	token_name: string,
-	price: string,
-	in_account: string,
-	in_account_eur: string,
-	vested_amount: string,
-	vested_amount_eur: string,
-	crowdloan_amount: string,
-	crowdloan_amount_eur: string,
-	total_amount: string,
-	total_amount_eur: string,
-	assets: string[]
+	id: string,
+	name: string,
+	tokens: PerPallet[],
+}
+
+interface PerChain {
+	name: string
+	accounts: PerAccount[]
 }
 
 const SUBSTRATE_BASED_CHAINS = JSON.parse(readFileSync('accounts.json').toString())
 
-function balance(api: ApiPromise, balance: BN): string {
-	return api.createType('Balance', balance).toHuman().toString()
-}
-
-async function priceOf(token: string): Promise<BN> {
+async function price_of(token: string): Promise<BN> {
 	try {
 		const data = await axios.get(`https://api.coingecko.com/api/v3/coins/${token}`);
 		const price = data.data['market_data']['current_price']['eur'];
@@ -38,113 +55,158 @@ async function priceOf(token: string): Promise<BN> {
 	}
 }
 
-function valueOf(api: ApiPromise, amount: BN, priceCent: BN): number {
-	const decimals = (new BN(10)).pow(new BN(api.registry.chainDecimals[0]))
-	return amount.mul(priceCent.div(new BN(100))).div(decimals).toNumber()
+async function fetch_vesting(api: ApiPromise, account: string, block_number: BN): Promise<PerPallet> {
+	const accountData = await api.query.system.account(account);
+	const vesting = (await api.query.vesting.vesting(account)).unwrapOrDefault();
+	const totalBalance = accountData.data.free.add(accountData.data.reserved);
+	assert.ok(vesting.length <= 1);
+	if (vesting.length === 1) {
+		const schedule = vesting[0];
+		const nonVested = totalBalance.sub(schedule.locked);
+		const madeFree = (block_number.sub(schedule.startingBlock)).mul(schedule.perBlock);
+		const leftVesting = schedule.locked.sub(madeFree);
+	}
+
+	return { assets: [], name: "vesting" }
 }
 
-function valueOfWithDecimals(api: ApiPromise, amount: BN, priceCent: BN, decimals: BN): number {
-	return amount.mul(priceCent.div(new BN(100))).div(decimals).toNumber()
+async function fetch_system(api: ApiPromise, account: string, token_name: string, price_cents: BN): Promise<PerPallet> {
+	const accountData = await api.query.system.account(account);
+	const decimals = new BN(api.registry.chainDecimals[0]);
+	const assets: Asset[] = [
+		{ name: `free_${token_name}`, price_cents, transferrable: true, amount: accountData.data.free, decimals  },
+		{ name: `reserved_${token_name}`, price_cents, transferrable: false, amount: accountData.data.reserved, decimals  }
+	];
+	return { assets, name: "system" }
 }
 
-function formatEUR(x: number): string {
-	return currencyFormatter.format(x, { locale: "nl-NL" })
+async function fetch_crowdloan(api: ApiPromise, account: string, token_name: string, price_cents: BN): Promise<PerPallet> {
+	const accountHex = api.createType('AccountId', account).toHex();
+	// @ts-ignore
+	const allParaIds: ParaId[] = (await api.query.paras.paraLifecycles.entries()).map(([key, _]) => key.args[0]);
+	let sum = new BN(0);
+	for (const id of allParaIds) {
+		const contribution = await api.derive.crowdloan.ownContributions(id, [accountHex])
+		// console.log(`cont to ${id}: ${Object.keys(contribution)}, ${Object.values(contribution)}`)
+		if (contribution[accountHex]) {
+			const contribution_amount = contribution[accountHex];
+			if (!contribution_amount.isZero()) {
+				sum = sum.add(contribution_amount);
+			}
+		}
+	}
+
+	const decimals = new BN(api.registry.chainDecimals[0]);
+	const assets: Asset[] = [{ name: token_name, price_cents, transferrable: false, amount: sum, decimals }]
+	return { assets, name: "crowdloan" }
+}
+
+async function fetch_crowdloan_rewards(api: ApiPromise, account: string, token_name: string, price_cents: BN): Promise<PerPallet> {
+	// really wacky way of decoding shit...
+	// @ts-ignore
+	const [_, total, claimed, _dont_care] = api.createType(
+		'(U8, Balance, Balance, Vec<AccountId32>)',
+		(await api.query.crowdloanRewards.accountsPayable(account)).toU8a(),
+	);
+	const locked = total.sub(claimed);
+
+	const decimals = new BN(api.registry.chainDecimals[0]);
+	return { assets: [ { name: token_name, price_cents, transferrable: false, amount: locked, decimals }], name: "crowdloanRewards" }
+}
+
+async function fetch_assets(api: ApiPromise, account: string): Promise<PerPallet> {
+	const assets: Asset[] = [];
+	const allAssetIds = (await api.query.assets.asset.entries()).map((a) => a[0].args[0]);
+	for (const assetId of allAssetIds) {
+		// @ts-ignore
+		const assetAccount: PalletAssetsAssetAccount = await api.query.assets.account(assetId, account);
+		if (!assetAccount.balance.isZero()) {
+			const meta = await api.query.assets.metadata(assetId);
+			const decimals = new BN(meta.decimals);
+			const name = (meta.symbol.toHuman() || "").toString().toLowerCase() ;
+			const price_cents = await price_of(name);
+			assets.push( { name, price_cents, transferrable: Boolean(assetAccount.isFrozen), amount: assetAccount.balance, decimals, })
+		}
+	}
+
+	return { assets, name: "assets" }
+}
+
+function display(api: ApiPromise, per_chain: PerChain) {
+	for (const account of per_chain.accounts) {
+		for (const pallet of account.tokens) {
+			for (const asset of pallet.assets) {
+				if (!asset.amount.isZero()) {
+					console.log(`📊 [${asset.transferrable? '🍺': '🔐'}] ${asset.name}::${pallet.name}::${account.name}: ${format_asset(api, asset.amount, asset.price_cents, asset.decimals)}`)
+				}
+			}
+		}
+	}
+}
+
+function format_asset(api: ApiPromise, amount: BN, price_cents: BN, decimals: BN): string {
+	const token_amount = api.createType('Balance', amount).toHuman().toString();
+	const token_amount_raw = amount.div(decimals)
+	const eur_amount = amount.mul(price_cents.div(new BN(100))).div(new BN(10).pow(decimals)).toNumber();
+	return `${token_amount} - ${currencyFormatter.format(eur_amount, { locale: "nl-NL" })}`
 }
 
 async function main() {
+	const all: PerChain[] = [];
 	for (const uri in SUBSTRATE_BASED_CHAINS) {
 		// @ts-ignore
-		const { chain, stashes, overwrite_currency_name }: { chain: string, overwrite_currency_name?: string, stashes: [string, string] } = SUBSTRATE_BASED_CHAINS[uri];
+		const {
+			chain,
+			stashes,
+			overwrite_currency_name,
+			pallets,
+		}: Chain = SUBSTRATE_BASED_CHAINS[uri];
 		const provider = new WsProvider(uri);
-		const api = await ApiPromise.create({ provider });
+		// @ts-ignore
+		const api = await ApiPromise.create({ provider, typesBundle: typesBundlePre900 });
 		const token_name = overwrite_currency_name ? overwrite_currency_name : chain.toLowerCase();
-		const priceCent = await priceOf(token_name);
-		const number = (await api.rpc.chain.getBlock()).block.header.number.toBn();
-		console.log(`✅ Connected to node: ${uri} / chain ${chain} / tokens ${api.registry.chainTokens} / at #${number}[ss58: ${api.registry.chainSS58}][current price :${priceCent} cents]`);
+		const price_cents = await price_of(token_name);
+		const block_number = (await api.rpc.chain.getBlock()).block.header.number.toBn();
+
+		console.log(`✅ Connected to node: ${uri} / chain ${chain} / decimals: ${api.registry.chainDecimals.toString()} / tokens ${api.registry.chainTokens} / at #${block_number} [ss58: ${api.registry.chainSS58}][current price :${price_cents} cents]`);
 
 
-		const perAccounts = [];
+		const chain_accounts = [];
 		for (const [account, name] of stashes) {
 			// account balance.
-			const accountData = await api.query.system.account(account);
-			const amount = new BN(accountData.data.free.add(accountData.data.reserved));
-			// console.log(`💸 Balance of ${name} ${balance(api, amount)}`)
+			const per_account: PerAccount = { tokens: [], id: account, name };
+
+			if (pallets.indexOf('system') > -1 && api.query.system ) {
+				const system = await fetch_system(api, account, token_name, price_cents);
+				per_account.tokens.push(system)
+			}
+
+			if (pallets.indexOf('crowdloanRewards') > -1 && api.query.crowdloanRewards ) {
+				const system = await fetch_crowdloan_rewards(api, account, token_name, price_cents);
+				per_account.tokens.push(system)
+			}
 
 			// vesting stuff
-			let vested = new BN(0);
-			if (api.query.vesting && api.query.vesting.vesting) {
-				const vesting = (await api.query.vesting.vesting(account)).unwrapOrDefault();
-				const totalBalance = accountData.data.free.add(accountData.data.reserved);
-				assert.ok(vesting.length <= 1);
-				if (vesting.length === 1) {
-					const schedule = vesting[0];
-					const nonVested = totalBalance.sub(schedule.locked);
-					const madeFree = (number.sub(schedule.startingBlock)).mul(schedule.perBlock);
-					const leftVesting = schedule.locked.sub(madeFree);
-					// assert.ok(nonVested.add(madeFree).eq(leftVesting));
-
-					// console.log(`🔐 of which ${balance(api, nonVested.add(madeFree))} is free, ${balance(api, leftVesting)} is still locked`)
-					vested = leftVesting;
-				}
+			if (pallets.indexOf('vesting') > -1 && api.query.vesting && api.query.vesting.vesting) {
+				const vesting = await fetch_vesting(api, account, block_number);
 			}
 
 			// crowdloan stuff
-			let crowdloan = new BN(0)
-			if (api.query.crowdloan) {
-				const accountHex = api.createType('AccountId', account).toHex();
-				// @ts-ignore
-				const allParaIds: ParaId[] = (await api.query.paras.paraLifecycles.entries()).map(([key, _]) => key.args[0]);
-				for (const id of allParaIds) {
-					const contribution = await api.derive.crowdloan.ownContributions(id, [accountHex])
-					if (contribution) {
-						const amount: BN = contribution[accountHex];
-						if (amount && amount.gt(new BN(0))) {
-							// console.log(`💰 found a contribution from ${account} to ${id} by ${balance(api, amount)}`);
-							crowdloan = crowdloan.add(amount);
-						}
-					}
-				}
+			if (pallets.indexOf('crowdloan') > -1 && api.query.crowdloan) {
+				const crowdloan = await fetch_crowdloan(api, account, token_name, price_cents);
+				per_account.tokens.push(crowdloan)
 			}
 
 			// assets
-			const assets: string[] = [];
-			if (api.query.assets) {
-				const allAssetIds = (await api.query.assets.asset.entries()).map((a) => a[0].args[0]);
-				for (const assetId of allAssetIds) {
-					const assetAccount = (await api.query.assets.account(assetId, account));
-					if (!assetAccount.balance.isZero()) {
-						const meta = await api.query.assets.metadata(assetId);
-						const decimals = new BN(meta.decimals);
-						const assetName = (meta.symbol.toHuman() || "").toString().toLowerCase() ;
-						const assetPriceCent = await priceOf(assetName);
-						const value = valueOfWithDecimals(api, assetAccount.balance, assetPriceCent, decimals);
-						assets.push(`${assetName}, ${assetAccount.balance.toHuman()}, ${formatEUR(value)}`)
-					}
-				}
+			if (pallets.indexOf('assets') > -1 && api.query.assets) {
+				const assets = await fetch_assets(api, account);
+				per_account.tokens.push(assets);
 			}
 
-			const perAccount: PerAccount = {
-				account_name: name,
-				token_name,
-				price: (priceCent.toNumber() / 100).toFixed(2),
-				in_account: balance(api, amount),
-				in_account_eur: formatEUR(valueOf(api, amount, priceCent)),
-				crowdloan_amount: balance(api, crowdloan),
-				crowdloan_amount_eur: formatEUR(valueOf(api, crowdloan, priceCent)),
-				vested_amount: balance(api, vested),
-				vested_amount_eur: formatEUR(valueOf(api, vested, priceCent)),
-				total_amount: balance(api, amount.add(crowdloan)),
-				total_amount_eur: formatEUR(valueOf(api, amount.add(crowdloan), priceCent)),
-				assets,
-			}
-			perAccounts.push(perAccount);
+			chain_accounts.push(per_account);
 		}
 
-		console.table(perAccounts)
-		const assetData = perAccounts.map((x) => x.assets).filter((x) => x.length);
-		if (assetData.length) {
-			console.table(assetData)
-		}
+		display(api, { accounts: chain_accounts, name: chain });
 	}
 }
 

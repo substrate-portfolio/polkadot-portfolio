8f7a2652e81ea7af32115ab8d7d5d3b6a3f013ae
kianenigma <kian@parity.io>
2022-03-23 21:10:02 +0000

    make it truly multi-account, and parallelize a bit more

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { PerAccount, PerChain, Summary } from "./types";
 import { priceOf } from "./utils";
 import yargs from 'yargs';
 import { hideBin } from "yargs/helpers"
+import BN from "bn.js";
 
 const optionsPromise = yargs(hideBin(process.argv))
 	.option('accounts', {
@@ -21,74 +22,77 @@ export const apiRegistry: Map<string, ApiPromise> = new Map();
 /**
  * Specification of a chain, as described in the JSON account file.
  */
-interface ChainSpec {
-	chain: string,
-	stashes: [string, string],
+interface AccountsConfig {
+	networks: string[],
+	stashes: [string, string][],
+}
+
+async function scrapeAccountFunds(account: string, name: string, nativeToken: string, price: number, api: ApiPromise): Promise<PerAccount> {
+	const perAccount = new PerAccount({ pallets: [], id: account, name });
+	try {
+		if (api.query.system) {
+			const system = await fetch_system(api, account, nativeToken, price);
+			perAccount.pallets.push(system)
+		}
+
+		if (api.query.crowdloanRewards) {
+			const system = await fetch_crowdloan_rewards(api, account, nativeToken, price);
+			perAccount.pallets.push(system)
+		}
+
+		if (api.query.vesting) {
+			// const vesting = await fetch_vesting(api, account, block_number);
+		}
+
+		if (api.query.crowdloan) {
+			const crowdloan = await fetch_crowdloan(api, account, nativeToken, price);
+			perAccount.pallets.push(crowdloan)
+		}
+
+		if (api.query.assets) {
+			const assets = await fetch_assets(api, account);
+			perAccount.pallets.push(assets);
+		}
+
+		if (api.query.tokens) {
+			const assets = await fetch_tokens(api, account);
+			perAccount.pallets.push(assets);
+		}
+
+		if (api.query.rewards) {
+			const assets = await fetch_reward_pools(api, account);
+			perAccount.pallets.push(assets);
+		}
+	} catch(e) {
+		// console.error(`error while fetching ${account}/${name} in network ${networkWs}`)
+	}
+
+	return perAccount
 }
 
 async function main() {
 	// initialize `apiRegistry`.
 	const options = await optionsPromise;
-	const accountConfig = JSON.parse(readFileSync(options.accounts).toString());
-	await Promise.all(Object.keys(accountConfig).map(async (uri: string) => {
+	const accountConfig: AccountsConfig = JSON.parse(readFileSync(options.accounts).toString());
+	await Promise.all(accountConfig.networks.map(async (uri: string) => {
 		const provider = new WsProvider(uri);
 		const api = await ApiPromise.create({ provider });
-		const chain = (await api.rpc.system.chain()).toString();
-		console.log(`✅ Connected to chain ${chain} / node: ${uri} / decimals: ${api.registry.chainDecimals.toString()} / tokens ${api.registry.chainTokens} / [ss58: ${api.registry.chainSS58}]`);
-		accountConfig[uri]["chain"] = chain.toLocaleLowerCase();
-		apiRegistry.set(chain.toLocaleLowerCase(), api);
+		console.log(`✅ Connected to ${uri} / decimals: ${api.registry.chainDecimals.toString()} / tokens ${api.registry.chainTokens} / [ss58: ${api.registry.chainSS58}]`);
+		apiRegistry.set(uri, api);
 	}));
 
 	const all_chains: PerChain[] = [];
-	for (const uri in accountConfig) {
-		const {
-			chain,
-			stashes,
-		}: ChainSpec = accountConfig[uri];
-		const api = apiRegistry.get(chain)!;
+	for (const networkWs of accountConfig.networks) {
+		const api = apiRegistry.get(networkWs)!;
+		const chain = (await api.rpc.system.chain()).toLowerCase();
 
 		const nativeToken = api.registry.chainTokens[0];
 		const price = await priceOf(nativeToken);
 
-		const chainAccounts = [];
-		for (const [account, name] of stashes) {
-			const perAccount = new PerAccount({ pallets: [], id: account, name });
-
-			if (api.query.system) {
-				const system = await fetch_system(api, account, nativeToken, price);
-				perAccount.pallets.push(system)
-			}
-
-			if (api.query.crowdloanRewards) {
-				const system = await fetch_crowdloan_rewards(api, account, nativeToken, price);
-				perAccount.pallets.push(system)
-			}
-
-			if (api.query.vesting) {
-				// const vesting = await fetch_vesting(api, account, block_number);
-			}
-
-			if (api.query.crowdloan) {
-				const crowdloan = await fetch_crowdloan(api, account, nativeToken, price);
-				perAccount.pallets.push(crowdloan)
-			}
-
-			if (api.query.assets) {
-				const assets = await fetch_assets(api, account);
-				perAccount.pallets.push(assets);
-			}
-
-			if (api.query.tokens) {
-				const assets = await fetch_tokens(api, account);
-				perAccount.pallets.push(assets);
-			}
-			if (api.query.rewards) {
-				const assets = await fetch_reward_pools(api, account);
-				perAccount.pallets.push(assets);
-			}
-
-			chainAccounts.push(perAccount);
-		}
+		const chainAccounts: PerAccount[] = [];
+		(await Promise.all(accountConfig.stashes.map(([account, name]) => {
+			return scrapeAccountFunds(account, name, nativeToken, price, api)
+		}))).forEach((perAccount) => chainAccounts.push(perAccount))
 
 		const chainData = new PerChain({ accounts: chainAccounts, name: chain });
 		console.log(`⛓ ${chain}:`)
